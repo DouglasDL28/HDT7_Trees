a99db9b6ac27b244f333c57af88c7c00ee96779e
DouglasDL28 <dele18037@uvg.edu.gt>
2019-03-21 23:33:31 -0600

    Test para InOrder y Add.

--- src/com/company/BinaryTreeTest.java
+++ src/com/company/BinaryTreeTest.java
@@ -1,16 +1,29 @@
 package com.company;
 
+import org.junit.Assert;
+
 import static org.junit.Assert.*;
 
 public class BinaryTreeTest {
 
     @org.junit.Test
     public void add() {
+        BinaryTree tree = new BinaryTree();
+        Association<String, String> test = new Association<>("Hello", "Hola");
 
+        tree.add(test);
+        System.out.println(tree.root.value);
     }
 
     @org.junit.Test
     public void traverseInOrder() {
+        BinaryTree tree = new BinaryTree();
 
+        tree.add(new Association<>("Hello", "Hola"));
+        tree.add(new Association<>("Goodbye", "Adios"));
+        tree.add(new Association<>("Dog", "perro"));
+        tree.add(new Association<>("Woman", "mujer"));
+
+        tree.traverseInOrder(tree.root);
     }
 }
